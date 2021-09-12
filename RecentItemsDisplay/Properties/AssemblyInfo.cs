e3c4d95c2dd549a6631e5d78a7914067e7fd4097
flibber-hk <76987839+flibber-hk@users.noreply.github.com>
2021-09-12 14:32:41 +0100

    Update AssemblyInfo.cs

--- RecentItemsDisplay/Properties/AssemblyInfo.cs
+++ RecentItemsDisplay/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("RecentItemsDisplay")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("RecentItemsDisplay")]
-[assembly: AssemblyCopyright("Copyright © HP Inc. 2021")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
